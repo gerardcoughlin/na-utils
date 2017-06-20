e410715f7107c1ade9841621e976a8bc45016180
gerrycoughlin <gcoughli@caltech.edu>
2017-06-20 11:28:46 -0700

    Updated dnatorna.py

--- dnatorna.py
+++ dnatorna.py
@@ -1,9 +1,51 @@
-"""Convert DNA sequence to RNA sequence, with same capitalization as DNA"""
+# %load dnatorna.py
+"""
+Convert DNA sequences to RNA.
+"""
 
 def rna(seq):
-    """Convert a DNA sequence to RNA"""
+    """
+    Convert a DNA sequence to RNA.
+    """
 
-    #Convert to uppercase
+    # Determine if original sequence was uppercase
+    seq_upper = seq.isupper()
+
+    # Convert to lowercase
+    seq = seq.lower()
+
+    # Swap out 't' for 'u'
+    seq = seq.replace('t', 'u')
+
+    # Return upper or lower case RNA sequence
+    if seq_upper:
+        return seq.upper()
+    else:
+        return seq
+
+
+def reverse_rna_complement(seq):
+    """
+    Convert a DNA sequence into its reverse complement as RNA.
+    """
+
+    # Determine if original was uppercase
+    seq_upper = seq.isupper()
+
+    # Reverse sequence
+    seq = seq[::-1]
+
+    # Convert to upper
     seq = seq.upper()
 
-    return seq.replace('T', 'U')
+    # Compute complement
+    seq = seq.replace('A', 'u')
+    seq = seq.replace('T', 'a')
+    seq = seq.replace('G', 'c')
+    seq = seq.replace('C', 'g')
+
+    # Return result
+    if seq_upper:
+        return seq.upper()
+    else:
+        return seq
